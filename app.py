27b4b0d56cf83037bd24588bd2dfaf762d8304bf
Eduis Galeas <ei.galeasv@uea.edu.ec>
2026-03-01 23:16:33 -0500

    Agregar base.html, about.html, index.html actualizado y rutas en app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ productos = []
 def inicio():
     return render_template("index.html", productos=productos)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")  # Página Acerca de
+
+@app.route("/productos")
+def productos_page():
+    return render_template("index.html", productos=productos)  # Lista de productos
+
 @app.route("/agregar", methods=["POST"])
 def agregar():
     nombre = request.form["nombre"]
